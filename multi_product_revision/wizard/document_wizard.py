c2e56a91ee0ad3dacfa610fad22292fb41db00aa
pravin <pravins@techspawn.co>
2020-12-01 20:38:53 +0530

    changes in multi_pro_revision

--- multi_product_revision/wizard/document_wizard.py
+++ multi_product_revision/wizard/document_wizard.py
@@ -1,18 +1,40 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
 
-
 # class Document_Wizard(models.TransientModel):
 class Document_Wizard(models.Model):
     _name = 'document.wizard'
     _description = 'Document Wizard'
 
-    item_number = fields.Integer(string="Item")
+    # # @api.onchange('item_number')
+    # def _get_item_numbers(self):
+    #     if self:
+    #         first_line_rec = self[0]            
+    #             for value, index in enumerate(first_line_rec.mrp_eco_product_id.document_wizard_line, start=1):
+    #                 index.update({'item_number':value})  
+
+    @api.onchange('item_number')
+    def _get_item_numbers(self):
+        if self:
+            first_line_rec = self[0] 
+            if first_line_rec.product_revisions_id:
+                for value, index in enumerate(first_line_rec.product_revisions_id.document_wizard_line, start=1):
+                    index.update({'item_number':value})  
+            elif first_line_rec.mrp_eco_product_id:
+                for value, index in enumerate(first_line_rec.mrp_eco_product_id.document_wizard_line, start=1):
+                    index.update({'item_number':value})  
+
+
+
+    item_number = fields.Integer(string="Item",compute='_get_item_numbers')
     upload_document_name = fields.Char(string="Upload Document Name")
     upload_document = fields.Binary(string="Upload Document")
     mrp_eco_product_id = fields.Many2one('mrp.eco.product',string='Mrp Eco Product Id')
     full_url = fields.Char(string="URL", compute='_compute_full_url')
-    product_revisions_id = fields.Many2one('product.revisions',string='Product Revisions Id')
+    product_revisions_id = fields.Many2one('product.revisions',string='Product Revisions Id')   
+        
+
+
 
 
     def create(self, vals_list):
