3d36c2b181dc1f6ea7c7122e385a8a10960a3db7
pravin <pravins@techspawn.co>
2021-01-05 20:19:13 +0530

    custom mail template added

--- auto_email_in_eco/model/mrp_eco.py
+++ auto_email_in_eco/model/mrp_eco.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, _
-
+import base64
 
 class inherit_MrpEco(models.Model):
     _inherit = 'mrp.eco'
@@ -39,18 +39,37 @@ class inherit_MrpEco(models.Model):
                     nondupli_email_to_list = [] 
                     [nondupli_email_to_list.append(x) for x in email_to_list if x not in nondupli_email_to_list]
 
+                    if company.phone:
+                        company_phone = company.phone
+                    else:
+                        company_phone = ""
+
+                    if company.email:
+                        company_email = company.email
+                    else:
+                        company_email = ""
+
+                    if company.website:
+                        company_website = company.website
+                    else:
+                        company_website = ""
+                    
                     template = mail_template.sudo().with_context(base_context,
                         eco_summuary_name = self.name,
                         eco_stage_name = self.stage_id.name,
                         email_from_usr = email_from_usr,
                         email_from_mail = email_from_mail,
                         # email_to_user = email_to_user,
-                        company = company.logo,
+                        # company = "data:image/png;base64,{}".format(company.logo),
+                        company_name = company.name,
+                        company_phone = company_phone,
+                        company_email = company_email,
+                        company_website = company_website,
                         email_from = email_from,
                         email_to = ",".join(nondupli_email_to_list),
-                        subject = ("ECO stage is changed "),
+                        subject = ("ECO (Ref {})".format(self.name)),                        
                         )         
                     template.send_mail(self.id, force_send=True)                                      
-                    self.message_post(message_type=_('comment'),body=_('''<p>Hello,</p>\n\n<p>User has changed ECO({}) stage to {}.</p>\n\n<p>Regards,</p><p>{}</p>'''.format(self.name,self.stage_id.name,email_from_usr)))                 
+                    self.message_post(message_type=_('comment'),body=_('''<p>Hello,</p>\n\n<p>Engineering Change Order<strong>({})</strong> has been moved to stage <strong>{}</strong>.</p><p>You can reply to this email if you have any questions.</p>\n\n<p>Thank you,</p><p>{}</p>'''.format(self.name,self.stage_id.name,email_from_usr)))                 
                     # template.send_mail(self.search([('name','=',self.name)]).id, force_send=True) #if onchange stage_id used
 
