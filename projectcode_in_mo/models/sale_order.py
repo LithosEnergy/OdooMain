b77d9585f2579ff4b17e4f3b42cd6f7ee58d0a40
pravin <pravins@techspawn.co>
2021-04-07 12:56:53 +0530

    change text to char

--- projectcode_in_mo/models/sale_order.py
+++ projectcode_in_mo/models/sale_order.py
@@ -4,14 +4,14 @@ from odoo import models,fields,api
 class SaleOrder(models.Model):
     _inherit ="sale.order"
 
-    project_code=fields.Text(string="Project Code")
+    project_code=fields.Char(string="Project Code")
 
-    @api.model
     def action_confirm(self):
         record = super(SaleOrder, self).action_confirm()
         mo=self.env['mrp.production'].search([('origin','=',self.name)])
-        for line in mo:
-            line.project_code=self.project_code
+        if mo:
+            for line in mo:
+                line.project_code=self.project_code
         return record
 
 
