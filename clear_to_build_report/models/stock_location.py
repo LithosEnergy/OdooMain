06d1b4551d0394029c4c93a24cbc10c74d339d59
jasonatsfour <jason@sfour.io>
2022-02-10 18:03:57 -0800

    Remove clear_to_build report
    Remove clear_to_build report

--- clear_to_build_report/models/stock_location.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class QualityLocation(models.Model):
-	_inherit = "stock.location"
-
-	x_preinspection = fields.Boolean('Pre Inspection Location')
